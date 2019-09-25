c113f1dc1e4ca1d04006c2c2ede84bc9e9f3ace8
TeddyBerard <berardteddy@icloud.com>
2019-09-25 18:42:45 +0200

    Initial commit

---
+++ Reciplease/Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Teddy Bérard on 25/09/2019.
+//  Copyright © 2019 Teddy Bérard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
